505afbca9665c38305cec692391790d22ec91e0a
miguel varas <miguelvaras456@gmail.com>
2022-04-10 13:17:10 -0300

    Update Quote.cs

--- Examen Practico/Examen Practico/Quote.cs
+++ Examen Practico/Examen Practico/Quote.cs
@@ -9,5 +9,27 @@ namespace Examen_Practico
 
     public class Quote
     {
+        public int NumberId{get; private set;}
+        public string DateHour{get; private set;}
+        public int SellerCode{get; private set;}
+        public int QuotedUnits{get; private set;}
+        public float QuotedResult{get; private set;}
+
+        public Garment quotedGarment;
+
+        public Quote(int numberId, string dateHour, int sellerCode, int quotedUnits, float quotedResult)
+        {
+            this.NumberId = numberId;
+            this.DateHour = dateHour;
+            this.SellerCode = sellerCode;
+            this.QuotedUnits = quotedUnits;
+            this.QuotedResult = quotedResult;
+        }
+
+        public void CalculateQuote()
+        {
+
+        }
+
     }
 }
